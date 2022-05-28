bc1c49d06e6b374112776316902d0bf902b082e0
Franz Noel Tanglao <franz@tanglao.org>
2022-05-27 20:16:31 -0700

    feat: Separate query for BE

--- pages/api/loans.ts
+++ pages/api/loans.ts
@@ -9,28 +9,37 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const client = getClient()
-  console.log('req', _req.query)
   try {
     const { page, pageSize } = _req.query
-
+    const offset = Number(pageSize) * Number(page)
     await client.connect()
-    const result = await client.query(`
-        select
-            t1.*,
-            t2.address_1,
-            t2.city,
-            t2.state,
-            t2.zip_code,
-            t3.name as company_name
-        from loan t1
-        left join address t2
-            on t1.address_id = t2.id
-        left join company t3
-            on t1.company_id = t3.id
-        limit ${pageSize} offset ${page}
-        `)
 
-    res.status(200).json([camelcaseKeys(result.rows), result.rowCount])
+    const recordCount = await client.query(`
+      select count(*) from loan t1
+      left join address t2
+          on t1.address_id = t2.id
+      left join company t3
+          on t1.company_id = t3.id
+    `)
+
+    const result = await client.query(`
+      select
+          t1.*,
+          t2.address_1,
+          t2.city,
+          t2.state,
+          t2.zip_code,
+          t3.name as company_name
+      from loan t1
+      left join address t2
+          on t1.address_id = t2.id
+      left join company t3
+          on t1.company_id = t3.id
+      order by id
+      limit ${pageSize} offset ${offset}
+    `)
+
+    res.status(200).json([camelcaseKeys(result.rows), recordCount.rows[0].count, pageSize, offset])
   } catch (err: any) {
       console.log(err)
     res.status(500).send(err.message)
